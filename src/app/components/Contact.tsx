13cb8e78c8b67d97087bebca8ca21f55f33f0846
Aayush Agnihotri <aa2328@cornell.edu>
2024-01-09 21:35:15 -0500

    Switching to calendly form

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Tooltip } from "@nextui-org/react";
-import { faEnvelope, faCalendarDays, faArrowUpRightFromSquare, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { InlineWidget } from "react-calendly";
 
 export default function Contact() {
   return (
@@ -9,7 +12,7 @@ export default function Contact() {
       <div className='mx-auto flex flex-col gap-5 items-center'>
         <div className='flex flex-col items-center text-center w-4/5'>
           <h1 className='text-4xl font-bold'>Contact Me</h1>
-          <p className='text-xl font-semibold text-gray-500'>Feel free to shoot me an email or schedule a coffee chat with the Calendly link below.</p>
+          <p className='text-xl font-semibold text-gray-500'>Feel free to shoot me an email or schedule a coffee chat through Calendly below.</p>
         </div>
 
         <div className='container mx-auto flex justify-center gap-5 flex-wrap lg:gap-20'>
@@ -22,37 +25,29 @@ export default function Contact() {
               </Tooltip>
             </div>
             <div className='flex justify-evenly md:justify-start'>
-              <Tooltip content='Calendly' showArrow={true} placement='left' closeDelay={0}>
-                <a href='https://calendly.com/aayush-agnihotri' className='hover:opacity-60 flex items-center' rel="noopener noreferrer" target="_blank">
-                  <FontAwesomeIcon icon={faCalendarDays} size='2x' />&nbsp; calendly.com/aayush-agnihotri &nbsp;<FontAwesomeIcon icon={faArrowUpRightFromSquare} size='2xs' />
-                </a>
-              </Tooltip>
-            </div>
-            <div className='flex justify-evenly md:justify-start'>
               <Tooltip content='LinkedIn' showArrow={true} placement='left' closeDelay={0}>
                 <a href='https://www.linkedin.com/in/aayush-agnihotri/' className='hover:opacity-60 flex items-center' rel="noopener noreferrer" target="_blank">
                   <FontAwesomeIcon icon={faLinkedin} size='2x' />&nbsp; linkedin.com/in/aayush-agnihotri &nbsp;<FontAwesomeIcon icon={faArrowUpRightFromSquare} size='2xs' />
                 </a>
               </Tooltip>
             </div>
+            <div className='flex justify-evenly md:justify-start'>
+              <Tooltip content='GitHub' showArrow={true} placement='left' closeDelay={0}>
+                <a href='https://github.com/Aayush-Agnihotri' className='hover:opacity-60 flex items-center' rel="noopener noreferrer" target="_blank">
+                  <FontAwesomeIcon icon={faGithub} size='2x' />&nbsp; github.com/Aayush-Agnihotri &nbsp;<FontAwesomeIcon icon={faArrowUpRightFromSquare} size='2xs' />
+                </a>
+              </Tooltip>
+            </div>
           </div>
 
           <div className='w-4/5 md:w-96'>
-            <form className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white">
-              <label className="text-gray-500 font-light mt-2">Full name<span className="text-red-500">*</span></label>
-              <input type="text" name="name" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-blue-500 font-light text-gray-500" />
-
-              <label  className="text-gray-500 font-light mt-4">Email<span className="text-red-500">*</span></label>
-              <input type="email" name="email" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-blue-500 font-light text-gray-500" />
-
-              <label  className="text-gray-500 font-light mt-4">Message<span className="text-red-500">*</span></label>
-              <textarea name="message" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-blue-500 font-light text-gray-500"></textarea>
-              <div className="mx-auto flex flex-row items-center justify-start">
-                <button className="px-10 mt-8 py-2 bg-blue-500 text-gray-50 rounded-md text-lg flex flex-row items-center font-bold hover:opacity-90">
-                  Send &nbsp;<FontAwesomeIcon icon={faPaperPlane} size='1x' />
-                </button>
-              </div>
-            </form>
+            <InlineWidget
+              url={"https://calendly.com/aayush-agnihotri"}
+              styles={{
+                height: "500px",
+                width: "100%",
+              }}
+            />
           </div>
         </div>
       </div>
