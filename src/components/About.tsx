fc52f2eba8bf09157ca2313d83d45d70ee462699
Aayush Agnihotri <aa2328@cornell.edu>
2024-01-10 15:50:20 -0500

    Changing to revealwrapper

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,36 @@
 "use client"
 
 import Terminal from "./Terminal"
-import { useRef } from "react"
-import { useIsVisible } from "@/hooks/useIsVisible"
+import { RevealWrapper } from "next-reveal"
 
 export default function About() {
-  const titleRef = useRef(null);
-  const titleIsVisible = useIsVisible(titleRef);
-  const contentRef = useRef(null);
-  const contentIsVisible = useIsVisible(contentRef);
-  const terminalRef = useRef(null);
-  const terminalIsVisible = useIsVisible(terminalRef);
 
   return (
     <div id='about' className='bg-gray-50 py-10 text-center'>
       <div className='w-4/5 mx-auto flex flex-col gap-5 items-center'>
         <div className='flex flex-col'>
           <div>
-            <h1 ref={titleRef} className={`text-4xl font-bold ${titleIsVisible ? "animate-in slide-in-from-bottom duration-500 fade-in" : ""}`}>About Me</h1>
+            <RevealWrapper>
+              <h1 className={`text-4xl font-bold`}>About Me</h1>
+            </RevealWrapper>
           </div>
           
           <div className='mt-5 flex flex-col gap-10 justify-center xl:flex-row xl:gap-20'>
-            <div ref={contentRef} className={`w-4/5 mx-auto text-center xl:w-1/3 ${contentIsVisible ? "animate-in slide-in-from-bottom duration-500 fade-in" : ""}`}>
+            <div className={`w-4/5 mx-auto text-center xl:w-1/3`}>
+              <RevealWrapper origin="bottom">
               <p className='text-xl text-gray-500'>I am currently a student at <span className='text-red-500'>Cornell University</span>, majoring in <span className='text-blue-500'>Computer Science</span> with a minor in <span className='text-blue-500'>Business</span>. I have experience in <span className='text-blue-500'>software engineering</span>, primarily in <span className='text-blue-500'>web and full stack development.</span></p>
               <br></br>
               <p className='text-xl text-gray-500'>I aim to leverage <span className='text-blue-500'>technology and data</span> to build <span className='text-blue-500'>useful and innovative</span> products.</p>
               <br></br>
               <p className='text-xl text-gray-500'>Aside from school and programming, I enjoy working out at the gym 🏋️, listening to music 🎵, or playing soccer ⚽.</p>
+              </RevealWrapper>
             </div>
 
-            <div ref={terminalRef} className={`min-h-unit-7xl max-h-unit-7xl overflow-x-hidden overflow-y-hidden hover:cursor-text bg-[#272B36] rounded-lg ${terminalIsVisible ? "animate-in slide-in-from-bottom duration-500 fade-in" : ""}`}>
-              <Terminal />
-            </div>
+            <RevealWrapper origin="bottom">
+              <div className={`min-h-unit-7xl max-h-unit-7xl overflow-x-hidden overflow-y-hidden hover:cursor-text bg-[#272B36] rounded-lg`}>
+                <Terminal />
+              </div>
+            </RevealWrapper>
           </div>
         </div>
       </div>
