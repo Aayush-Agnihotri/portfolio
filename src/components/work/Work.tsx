d633ddcedff4561eb5bf705decbe1a735fa996d1
Aayush Agnihotri <aa2328@cornell.edu>
2024-01-10 19:29:50 -0500

    Adding transitions to work experience

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,12 +1,20 @@
 "use client"
 
 import WorkAccordion from './WorkAccordion'
+import { useRef } from "react"
+import { useIsVisible } from "@/hooks/useIsVisible"
 
 export default function Work() {
+  const headerRef = useRef(null);
+  const headerIsVisible = useIsVisible(headerRef);
+
   return (
     <div className='flex flex-col py-10'>
       <div className='flex flex-col items-center mb-5 text-center'>
-        <h1 className={`text-4xl font-bold`}>Work Experience</h1>
+        <h1 ref={headerRef} className={`text-4xl font-bold
+        ease-in transition-all duration-1000
+        ${headerIsVisible ? "translate-y-0" : "translate-y-20"}
+        ${headerIsVisible ? "opacity-100" : "opacity-0"}`}>Work Experience</h1>
       </div>
 
       <div className='container w-4/5 mx-auto'>
--- src/components/work/WorkAccordion.tsx
+++ src/components/work/WorkAccordion.tsx
@@ -5,6 +5,8 @@ import { Accordion, AccordionItem } from "@nextui-org/react";
 import { WorkData } from "./WorkData";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { useRef } from "react";
+import { useIsVisible } from "@/hooks/useIsVisible";
 
 export interface WorkDataInterface {
   company: string;
@@ -15,26 +17,32 @@ export interface WorkDataInterface {
 }
 
 export default function WorkAccordion() {
+  const accordionRef = useRef(null);
+  const accordionIsVisible = useIsVisible(accordionRef);
 
   return (
-    <Accordion selectionMode="multiple" variant="splitted" defaultExpandedKeys={["0"]}>
-      {WorkData.map((data, i) => (
-        <AccordionItem  
-          key={i}
-          aria-label={`${data.company}`}
-          subtitle={`${data.role}`}
-          title={`${data.company}`}
-          startContent={<FontAwesomeIcon icon={data.icon} />}
-          classNames={{ subtitle: "text-blue-500" }}
-          indicator={<FontAwesomeIcon icon={faChevronLeft} />}
-        >
-          <p className='italic'>{data.date}</p>
-          {data.details.map((detail, i) => (
-            <li key={i}>{detail}</li>
-          ))}
-          <p className='pb-3'></p>
-        </AccordionItem>
-      ))}
-    </Accordion>
+    <div ref={accordionRef} className={`ease-in transition-all duration-1000
+        ${accordionIsVisible ? "translate-y-0" : "translate-y-20"}
+        ${accordionIsVisible ? "opacity-100" : "opacity-0"}`}>
+      <Accordion selectionMode="multiple" variant="splitted" defaultExpandedKeys={["0"]}>
+        {WorkData.map((data, i) => (
+            <AccordionItem  
+              key={i}
+              aria-label={`${data.company}`}
+              subtitle={`${data.role}`}
+              title={`${data.company}`}
+              startContent={<FontAwesomeIcon icon={data.icon} />}
+              classNames={{ subtitle: "text-blue-500" }}
+              indicator={<FontAwesomeIcon icon={faChevronLeft} />}
+            >
+              <p className='italic'>{data.date}</p>
+              {data.details.map((detail, i) => (
+                <li key={i}>{detail}</li>
+              ))}
+              <p className='pb-3'></p>
+            </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
   );
 }
